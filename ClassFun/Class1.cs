36f7fdf11338a9b5a7a91dab0f4c5aca6dc235af
P8ON-code <papasteryd8@gmail.com>
2022-12-06 16:15:33 +0100

    Dodaj pliki projektów.

---
+++ ClassFun/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFun
+{
+    internal class Class1
+    {
+        public Class1(string name) 
+        {
+            Console.WriteLine("Hello my name is {0}, it's nice to meet you!", name);
+        }
+    }
+}
---
+++ ClassFun/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassFun
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Object = new Class1("Object1");
+        }
+    }
+}
